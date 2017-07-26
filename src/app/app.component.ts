1320715ca3cbf8dc0d796da3829172c5ad450abc
Miguel <maldesan@gmail.com>
2017-07-26 01:33:31 -0400

    basic authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from './session.service';
 
 import { QuizComponent } from './quiz/quiz.component'
 
@@ -7,5 +8,65 @@ import { QuizComponent } from './quiz/quiz.component'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    user: any;
+    formInfo = {
+    username: '',
+    password: ''
+  };
+
+  // user: any;
+  error: string;
+  privateData: any ='';
+
+  constructor(private session: SessionService) { }
+
+  ngOnInit() {
+    this.session.isLoggedIn()
+      .subscribe(
+        (user) => this.successCb(user)
+      );
+  }
+
+  login() {
+    this.session.login(this.formInfo)
+      .subscribe(
+        (user) => this.user = user,
+        (err) => this.error = err
+      );
+  }
+
+  signup() {
+    this.session.signup(this.formInfo)
+      .subscribe(
+        (user) => this.user = user,
+        (err) => this.error = err
+      );
+  }
+
+  logout() {
+    this.session.logout()
+      .subscribe(
+        () => this.user = null,
+        (err) => this.error = err
+      );
+  }
+
+  getPrivateData() {
+    this.session.getPrivateData()
+      .subscribe(
+        (data) => this.privateData = data,
+        (err) => this.error = err
+      );
+  }
+
+  errorCb(err) {
+    this.error = err;
+    this.user = null;
+  }
+
+  successCb(user) {
+    this.user = user;
+    this.error = null;
+  }
 }
